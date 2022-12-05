09c7fff8e87c62643993c93f4d41c0c0c74d0206
Thomas <76893724+thomastubby@users.noreply.github.com>
2022-12-05 02:28:39 -0500

    Update bscdia.sh
    -added more DNS information, organized information

--- bscdia.sh
+++ bscdia.sh
@@ -1,78 +1,91 @@
 #by: Tom Stein
 
 #!/bin/bash
+showDate=(date +"%m-%d-%Y @ %H:%M:%S")
 user="root"
 echo "What is the user? (default: root)"
 read username
-if [ "$username" = "" ];
-	then username="root"
+if [ "$username" = "" ]; then
+	username="root"
 fi
-echo "Please input key file location (default: blank)"
+echo "Please input key file location (default: none)"
 read keyInput
-if [ "$keyInput" = "" ];
-	then keyLocation=""
+if [ "$keyInput" = "" ]; then
+	keyInput="None selected" &&
+	keyLocation=""
 else keyLocation="-i $keyInput"
 fi
 
 echo "User: $username"
 echo "Key: $keyInput"
-echo "What is the IP of the server? "
+echo "What is the IP/domain of the server? "
 read userip
-showDate=(date +"%m-%d-%Y @ %H:%M:%S")
+ipcheck=$(echo $userip | grep -E ".+\..+\..+\.+")
+if [ "$ipcheck" != "$userip" ]; then
+	domain="$userip"
+else domain="Not provided"
+fi
+getRecords=$(dig $domain | grep -E ".+\s+.+\s.+\s+.+\..+\..+\..+" | grep -v "<<>>")
+if [ "$domain" == "Not provided" ]; then
+	getRecords="Not provided"
+fi
 
-ssh -tt $username@$userip $keyLocation <<\EOF
-
-##HIDES INPUT
+## HERE UNTIL SYSINFO ESTABLISHES CONNECTION TO SERVER AND CLEARS UNWANTED INFORMATION FROM TERMINAL
+{ ssh -tt $username@$userip $keyLocation <<\EOF_remote
+clear
 stty -echo
-
-##SYS INFO
+clear
+echo "Gathering information, this could take a minute..."
+## SYSTEM INFO
 cpuName=$(lscpu | grep "Model name:" | xargs)
 cpuCores=$(lscpu | grep "CPU(s):" | grep -v "NUMA" | xargs)
 gpuName=$(lspci | grep "VGA compatible controller" | cut -d " " -f 5-)
 ram=$(free -h | grep "Mem:" | xargs)
 hdd=$(df -h | grep "/dev/")
-publicip=$(curl -s ifconfig.me && echo | xargs)
-localip=$(ip a | grep -E ".: .+:|.+\..+\..+\..+/..|inet6")
 user=$(whoami)
 workingOS=$(cat /etc/os-release | grep -A 1 -w "NAME=" | cut -d "\"" -f 2)
 showDate=$(date +"%m-%d-%Y @ %H:%M:%S")
 uptime=$(uptime -p)
 
-##NETWORK
+## NETWORK
 checkGip=$(ping -c 1 8.8.8.8 | grep "1 received," | xargs | cut -d " " -f 4,5)
 checkCip=$(ping -c 1 1.1.1.1 | grep "1 received," | xargs | cut -d " " -f 4,5)
 checkGdom=$(ping -c 1 google.com | grep "1 received," | xargs | cut -d " " -f 4,5)
 checkCdom=$(ping -c 1 cloudflare.com | grep "1 received," | xargs | cut -d " " -f 4,5)
+localip=$(ip a | grep -E ".: .+:|.+\..+\..+\..+/..|inet6")
+publicip=$(curl -s ifconfig.me && echo | xargs)
+getRecordsLocal=$(dig $publicip | grep -E ".+\s+.+\s.+\s+.+\..+\..+\..+" | grep -v "<<>>")
+
 ## IP CHECK
-if [ "$checkGip" = "1 received," ];
-        then Gip="success"
+if [ "$checkGip" = "1 received," ]; then
+	Gip="success"
 else Gip="failure"
 fi
-if [ "$checkCip" = "1 received," ];
-        then Cip="success"
+if [ "$checkCip" = "1 received," ]; then
+	Cip="success"
 else Cip="failure"
 fi
-if [ $Gip = "success" ] && [ $Cip = "success" ];
-        then networkStatus="Network success"
+if [ $Gip = "success" ] && [ $Cip = "success" ]; then
+	networkStatus="Network success"
 else networkStatus="Partial network failure"
 fi
 
-##DNS CHECK
-if [ "$checkGdom" = "1 received," ];
-        then gDom="success"
+## DNS CHECK
+if [ "$checkGdom" = "1 received," ]; then
+	gDom="success"
 else gDom="failure"
 fi
-if [ "$checkCdom" = "1 received," ];
-        then cDom="success"
+if [ "$checkCdom" = "1 received," ]; then
+	cDom="success"
 else cDom="failure"
 fi
-if [ $gDom = "success" ] && [ $cDom = "success" ];
-        then dnsStatus="DNS success"
+if [ $gDom = "success" ] && [ $cDom = "success" ]; then
+	dnsStatus="DNS success"
 else dnsStatus="DNS failure"
 fi
 
 clear
-cat <<limit
+cat <<EOF_local
 |===========================================================|
 | ________  ________  ________  ________  ___  ________     |
 ||\   __  \|\   ____\|\   ____\|\   ___ \|\  \|\   __  \    |
@@ -113,10 +126,25 @@ Google DNS: $gDom
 Cloudflare DNS: $cDom
 -=$dnsStatus=-
 
+DNS Records (for $publicip):
+$getRecordsLocal
+
 Public: $publicip
-Interfaces (IPv4/IPv6 only): 
+Interfaces (IPv4/IPv6 only):
 $localip
 -------------
-limit
+EOF_local
 logout
-EOF
+EOF_remote
+} &&
+
+## METADATA - Anything past this line provides more information about the server itself that isnt or cant be pulled from the system.
+{ cat <<EOF_meta
+
+-=-=METADATA=-=-
+DNS records (of domain, if provided):
+-------------
+$getRecords
+-------------
+EOF_meta
+}
